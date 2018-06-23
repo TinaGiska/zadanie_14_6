bd2917e08bbf8c8ecfdd4e1bbc369dfb6664dfbf
Tina Giska <tina.tylicka@gmail.com>
2018-06-23 19:27:14 +0200

    Update task

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ var Counter = React.createClass({
        });
    },
 
+   decrement: function() {
+      this.setState({
+          counter: this.state.counter - 1
+      });
+  },
+
     render: function() {
-        return React.createElement('div', {onClick: this.increment},
-               React.createElement('span', {}, 'Licznik ' + this.state.counter),
-               React.createElement('button', {type: 'submit'}, "Dodawanie")
-        );
+
+      return React.createElement('div',{},
+             React.createElement('p', {}, 'Licznik ' + this.state.counter),
+             React.createElement('button', {onClick: this.increment}, "Dodawanie"),
+             React.createElement('button', {onClick: this.decrement}, "Odejmowanie"),
+           )
     }
 });
 
 var element = React.createElement(Counter);
+var element2 = React.createElement(Counter);
+var element3 = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
